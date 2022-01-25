ea41d7b9a4308dae867843d3cbb9bc6d8ab0f248
Karl Damus <getaholdofkarl@gmail.com>
2022-01-25 00:23:08 -0500

    Update Constants.java
    Added swing elements:
    - colours
    - sizing
    - border
    
    Updated comments and constant names
    
    Added dev mode variable

--- src/game/shared/Constants.java
+++ src/game/shared/Constants.java
@@ -1,7 +1,38 @@
 package game.shared;
 
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
 public interface Constants {
+	// UI
+	String GAME_TITLE = "A Wordle Clone by Karl Damus";
+	
+	// COLOURS
+	Color WHITE = Color.decode("#ffffff");
+	Color GREEN = Color.decode("#538d4e");
+	Color YELLOW = Color.decode("#b59f3b");
+	Color LIGHT_GREY = Color.decode("#818384");
+	Color DARK_GREY = Color.decode("#3a3a3c");
+	Color BACKGROUND = Color.decode("#000000");
+	
+	// BORDERS
+	Border J_LABEL_BORDER = BorderFactory.createLineBorder(WHITE, 5);
+	
+	// SIZING
+	int CHAR_BOX_HEIGHT = 100; 
+	int CHAR_BOX_WIDTH = 100;
+	int MIN_WINDOW_HEIGHT = 700;
+	int MIN_WINDOW_WIDTH = 500;
+	
+	
+	// BACKEND
 	int WORD_LENGTH = 5;
-	int ALLOWED_NUM_GUESSES = 6;
-	String WORDS_LIST = "src/game.txt/words.game.txt";
+	int NUM_ALLOWED_GUESSES = 6;
+	
+	// FILES
+	String WORDS_LIST = "src/game/txt/words.txt";
+	
+	// DEV ZONE
+	boolean DEV_MODE = true;
 }
