77ad94997b2d635fd739b98261ef80e24410fa1a
Karl Damus <getaholdofkarl@gmail.com>
2022-01-25 14:57:45 -0500

    Update comments & removed old code

--- src/game/shared/Guess.java
+++ src/game/shared/Guess.java
@@ -14,10 +14,9 @@ import java.util.Map;
  *          <li>0 => character is not in the word</li>
  *          <li>1 => character is in the word but not that spot</li>
  *          <li>2 => character is in the word and in that spot</li>
+ * <br>         
  * Issues:
- * 
- * Documentation Link:
- * N/A
+ *      <li>first-check superiority ... see to-do</li>
  */
 public class Guess {
 	private ArrayList<Map<Integer, Character>> guess;
@@ -26,78 +25,71 @@ public class Guess {
 	private static final int IN_WRONG_POS = 1;
 	private static final int IN_RIGHT_POS = 2;
 
-	// TODO: implement number of letters checking
-	
+	/**
+	 * Create a Guess object based on the correct word
+	 * @param currentGuess the String value of the user's guess
+	 * @param word the current word that needs to be guessed
+	 */
 	public Guess(String currentGuess, String word) {
-		guess = new ArrayList<Map<Integer, Character>>();
-		ArrayList<Character> wordAsList = getCharListFromString(word);
-		
+		guess = new ArrayList<Map<Integer, Character>>(); // initialize the guess ArrayList
+		ArrayList<Character> wordAsList = getCharListFromString(word); // get charList from the word to guess
+
+		/**
+		 * Loop through each character in the currentGuess
+		 */
 		for (int i = 0; i < currentGuess.length(); i++) {
-			char currentChar = currentGuess.charAt(i);
-			Map<Integer, Character> map = new HashMap<>();
+			char currentChar = currentGuess.charAt(i); // get character at index i
+			Map<Integer, Character> map = new HashMap<>(); // initialize a new Map for the guess ArrayList
 			
-			// character is not in word
+			// CASE 0: if character is not in word
 			if (!wordAsList.contains(currentChar)) {
 				map.put(NOT_IN_WORD, currentChar);
 				guess.add(map);
 			} 
-			// character is in word but in wrong position
+			// CASE 1: if character is in word but in wrong position
 			else if (wordAsList.contains(currentChar) && wordAsList.get(i) != currentChar) {
 				// duplicate letter check
 				int duplicates = 0;
 				int numGuessesOfSameLetter = 0;
+				
+				// check how many duplicates are in the word
 				for (Character c : wordAsList) {
 					if (String.valueOf(c).equalsIgnoreCase(String.valueOf(currentChar)))
 						duplicates++;
 				}
+				// check how many times the character has been guessed
 				for (int k = 0; k < guess.size(); k++) {
 					if (guess.get(k).containsValue(currentChar))
 						numGuessesOfSameLetter++;
 				}
-				System.out.println(numGuessesOfSameLetter);
 				
+				// TODO: check duplicates after to avoid first-check superiority
+				
+				// if number of guesses hasn't reached the 
+				// total number of duplicates
 				if (numGuessesOfSameLetter < duplicates) {
 					map.put(IN_WRONG_POS, currentChar);
 					guess.add(map);
-				} else {
+				} 
+				// else, duplicates are all filled up
+				else {
 					map.put(NOT_IN_WORD, currentChar);
 					guess.add(map);
 				}
-				
-				
-//				// if only one
-//				if (duplicates == 1) {
-//					for (int j = 0; j < guess.size(); j++) {
-//						// if guess already contains the currentChar
-//						// add as NOT_IN_WORD key
-//						if (guess.get(j).containsValue(currentChar)) {
-//							map.put(NOT_IN_WORD, currentChar);
-//							guess.add(map);
-//							break;
-//						}
-//					}
-//					// else, this guess hasn't ben made yet
-//					// add as IN_WRONG_POS key
-//					map.put(IN_WRONG_POS, currentChar);
-//					guess.add(map);
-//				} 
-//				// else one values of currentChar
-//				else {
-////					map.put(IN_WRONG_POS, currentChar);
-////					guess.add(map);
-//				}
-				
 			} 
-			// character is in word and in right position
+			// CASE 2: if character is in word and in right position
 			else if (wordAsList.contains(currentChar) && wordAsList.get(i) == currentChar) {
 				map.put(IN_RIGHT_POS, currentChar);
 				guess.add(map);
 			}
-
-			System.out.println(map.entrySet());
 		}
 	}
-	
+
+	/**
+	 * Given a String, generate an ArrayList of Characters
+	 * @param s the input String
+	 * @return ArrayList of Characters from the input String
+	 */
 	public ArrayList<Character> getCharListFromString(String s) {
 		ArrayList<Character> chars = new ArrayList<>();
 		
