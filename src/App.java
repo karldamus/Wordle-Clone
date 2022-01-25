b36232cb3cea3ff637821525e5e1ba4c4e991e39
Karl Damus <getaholdofkarl@gmail.com>
2022-01-25 00:18:08 -0500

    Update App.java
    Remove useless methods

--- src/App.java
+++ src/App.java
@@ -17,16 +17,4 @@ public class App {
 	public static void main(String[] args) throws IOException {
 		Game game = new Game();
 	}
-
-	protected static void printf(String format, Object... args) {
-		System.out.printf(format, args);
-	}
-
-	protected static void println(Object message) {
-		System.out.println(message);
-	}
-
-	protected static void print(Object message) {
-		System.out.print(message);
-	}
 }
